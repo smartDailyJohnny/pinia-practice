9398557f48ef619b87958cb3f01118698157f44d
Johnny <johnny.chang@smartdaily.com.tw>
2023-08-09 16:15:30 +0800

    getters

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
-import TaskDetails from '@/components/TaskDetails.vue'
+import { ref } from 'vue';
 import { useTaskStore } from '@/stores/TaskStore'
+import TaskDetails from '@/components/TaskDetails.vue'
 
 const taskStore = useTaskStore()
+const filter = ref('all')
 </script>
 
 <template>
@@ -13,9 +15,23 @@ const taskStore = useTaskStore()
       <h1>Pinia Tasks</h1>
     </header>
 
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter === 'all'">All Tasks</button>
+      <button @click="filter === 'favs'">Fav Tasks</button>
+    </nav>
+
     <!-- task list -->
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
+      <p>All Tasks</p>
       <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>Fav Tasks</p>
+      <div v-for="task in taskStore.favs">
         <TaskDetails :task="task" />
       </div>
     </div>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -6,6 +6,11 @@ export const useTaskStore = defineStore('taskStore', {
             { id: 1, title: "buy some milk", isFav: false },
             { id: 2, title: "play Gloomhaven", isFav: true }
         ]
-    })
+    }),
+    getters: {
+        favs() {
+            return this.tasks.filter((item) => item.isFav)
+        }
+    }
 })
 
